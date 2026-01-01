385cfa742e1fc4eb7c9ac32fa7ebed855aa0d18a
Yura Chepurnykh <yura.chepurnykh@gmail.com>
2026-01-01 14:32:12 -0500

    feat(init_lua): add basic options

---
+++ init.lua
@@ -0,0 +1,9 @@
+-- basic options
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtap = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+
